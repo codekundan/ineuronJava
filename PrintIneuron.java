4c2eb472cb8b17f68b0407b67e31bdd0aa12169d
codekundan <codekundannegi@gmail.com>
2022-10-25 16:40:25 +0530

    Assignment1

---
+++ PrintIneuron.java
@@ -0,0 +1,257 @@
+package ineuron;
+
+public class PrintIneuron {
+   public static void main(String[] args) {
+	   int i;
+	   
+	   for(i=0; i<7; i++) {               // I
+		   if(i==0 || i==6) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                  // N
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {                 //E
+		   if(i==6) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {              //U
+		   if(i==0 || i==5) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {              //R
+		   if(i==4 || i==5) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {                   // O
+		   if(i==0 || i==4 || i==5) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   System.out.println("");
+	   for(i=0; i<7; i++) {
+		   if(i==3) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {
+		   if(i==0 || i==1 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {
+		   if(i==0) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {
+		   if(i==0 || i==5) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {                   // O
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==1 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   System.out.println("");
+	   for(i=0; i<7; i++) {
+		   if(i==3) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==2 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {                 //E
+		   if(i==0 || i==1 || i==2) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {
+		   if(i==0 || i==5) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {              //R
+		   if(i==0 || i==1 || i==2) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {                   // O
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==2 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   System.out.println("");
+	   for(i=0; i<7; i++) {
+		   if(i==3) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==3 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {
+		   if(i==0) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {
+		   if(i==0 || i==5) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {                   // O
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                // N
+		   if(i==0 || i==3 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   System.out.println("");
+	   for(i=0; i<7; i++) {               // I
+		   if(i==0 || i==6) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                  // N
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {                 //E
+		   if(i==6) {
+			   System.out.print(" ");
+		   } else {
+			   System.out.print("*");
+		   }
+	   }
+	   for(i=0; i<=6; i++) {              //U
+		   if(i==1 || i==2 || i==3 ||i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<=5; i++) {                   // O
+		   if(i==1 || i==2 || i==3 ) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+	   for(i=0; i<6; i++) {                  // N
+		   if(i==0 || i==4) {
+			   System.out.print("*");
+		   } else {
+			   System.out.print(" ");
+		   }
+	   }
+   }
+}
